820d9b8d25cb6a11fce39437866d2cd250194b87
Tadesse Jemal <tadessejemal63@gmail.com>
2023-01-03 20:26:42 +0300

    fix linter error

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import React from 'react'
-import './App.css';
-import Greeting from './components/Greeting';
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import Greeting from './components/Greeting';
 import store from './redux/configureStore';
 
 function App() {
   return (
-      <React.StrictMode>
-        <Provider store={store}>
-          <Router>
-            <Routes>
-              <Route path="/" element={<Greeting />} />
-            </Routes>
-          </Router>
-        </Provider>
-      </React.StrictMode>
+    <React.StrictMode>
+      <Provider store={store}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Greeting />} />
+          </Routes>
+        </Router>
+      </Provider>
+    </React.StrictMode>
   );
 }
 

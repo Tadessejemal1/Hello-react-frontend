25f87e31531282a4b2321862f2a65670a9f5eace
Tadesse Jemal <tadessejemal63@gmail.com>
2023-01-03 19:06:13 +0300

    add react components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Greeting from './components/Greeting';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <React.StrictMode>
+        <Provider store={store}>
+          <Router>
+            <Routes>
+              <Route path="/" element={<Greeting />} />
+            </Routes>
+          </Router>
+        </Provider>
+      </React.StrictMode>
   );
 }
 
---
+++ src/components/Greeting.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchGreetingAPI } from '../redux/reducers/greetingReducer';
+
+const Greeting = () => {
+  const dispatch = useDispatch();
+  const message = useSelector((state) => state.messages);
+
+  useEffect(() => {
+    dispatch(fetchGreetingAPI());
+  }, [dispatch]);
+
+  const handleClick = () => {
+    dispatch(fetchGreetingAPI());
+  };
+
+  return (
+    <>
+      <h1 style={{ color: 'blue' }}>{message}</h1>
+      <button type="button" onClick={handleClick}>
+        Refresh
+      </button>
+    </>
+  );
+};
+
+export default Greeting;
